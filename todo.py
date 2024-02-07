092405ef9e750f8cd8ac2fd97315d4e60d277c23
kbirgelis <149561984+kbirgelis@users.noreply.github.com>
2024-02-07 10:19:45 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -7,46 +7,71 @@
 
 def add(list, item):
   # https://www.w3schools.com/python/python_lists_add.asp
-  pass
+ list.append(super_shop)
+pass
 
 
 def remove(list, index):
-  # https://www.w3schools.com/python/python_lists_remove.asp
-  pass
+
+ list.pop(index)
+
+pass
 
 
 def clear(list):
-  # https://www.w3schools.com/python/python_lists_remove.asp
-  pass
+ 
+ list.clear()
+ 
+
+pass
 
 
 def print_list(list):
-  # https://www.w3schools.com/python/python_lists_loop.asp
+  if list == 0:
+    print("List is empty")
+  else:
+   print(list)
   pass
 
 
 def show(list):
-  # https://www.w3schools.com/python/python_lists_access.asp
+  print(list[index])
   pass
 
 list = []
+super_shop = []
+
 print("List is empty now, what you want to do?")
 while True:
-  choice = int(input("1. Add\n2. Remove\n3. Clear\n4. Print list\n5. Show item by index"))
-  if choice == 1:
-    item = input("What you want to add?\n")
-    add(list, item)
-    print_list(list)
+
+  print("....")
+  print("....")
+  
+  choice = int(input("1. Add\n2. Remove\n3. Clear\n4. Print list\n5. Show item by index "))
+
+  if len(list) > 10:
+   print("The list can't be longer than 10 elements!")
+   list.clear()
+
+   if choice == 1:
+    super_shop = input("What you want to add?\n")
+    list.append(super_shop)
+    print(list)
+
   elif choice == 2:
     index = int(input("What you want to remove?\n"))
     remove(list, index)
-    print_list(list)
+    print(list)
+
   elif choice == 3:
     clear(list)
-    print_list(list)
+    print(list)
+
   elif choice == 4:
-    print_list(list)
+    print(list)
+
   elif choice == 5:
     show(list)
+
   else:
     print("Invalid input")
